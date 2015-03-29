fdcc878eeadbf925c0eff4b0f18cccacbdb79bfc
leoandbob <aidandegooyer@gmail.com>
2015-03-29 13:04:27 -0400

    Update extension.js

--- extension.js
+++ extension.js
@@ -75,7 +75,7 @@
     //Change the bots default settings and make sure they are loaded on launch
 
     localStorage.setItem("basicBotsettings", JSON.stringify({
-        botName: "WolfBot",
+        botName: "Plug.DJ Robot",
         language: "english",
         chatLink: "https://rawgit.com/Yemasthui/basicBot/master/lang/en.json",
         startupCap: 1, // 1-200
